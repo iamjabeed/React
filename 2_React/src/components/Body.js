a38e470fef2414f9b7fff0c0f5ba01772d731410
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-02-24 21:06:46 +0530

    Filter data

--- 2_React/src/components/Body.js
+++ 2_React/src/components/Body.js
@@ -1,5 +1,6 @@
 import { RestaurentList } from "./constants";
 import IMG_URL_CDN from "./constants";
+import { useState } from "react";
 const RestaurentCard = ({
   cloudinaryImageId,
   name,
@@ -23,13 +24,46 @@ const RestaurentCard = ({
   );
 };
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant?.data?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return filterData;
+}
+
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(RestaurentList);
+
   return (
-    <div className="restaurent-card-List">
-      {RestaurentList.map((restaurent) => {
-        return <RestaurentCard key={restaurent.data.id} {...restaurent.data} />;
-      })}
-    </div>
+    <>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search a restaurant you want..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      ></input>
+      <button
+        className="search-btn"
+        onClick={() => {
+          // filter the data
+          const data = filterData(searchText, restaurants);
+          // update the state of restaurants list
+          setRestaurants(data);
+        }}
+      >
+        Search
+      </button>
+
+      <div className="restaurent-card-List">
+        {restaurants.map((restaurent) => {
+          return (
+            <RestaurentCard key={restaurent.data.id} {...restaurent.data} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 export default Body;
